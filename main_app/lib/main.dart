dc177a3040c58c8920bde3f7c693a9c060c07947
Mohamed Fawzy <mohamed_2100699@science.sohag.edu.eg>
2025-04-11 01:48:10 +0200

    Initialize project structure and create base files

--- main_app/lib/main.dart
+++ main_app/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:e_commerce_app/app/screens/auth_screen.dart';
 import 'package:e_commerce_app/components/declerations.dart';
+import 'package:e_commerce_app/router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -22,10 +24,26 @@ class MyApp extends StatelessWidget {
           iconTheme: IconThemeData(color: Colors.black),
         ),
       ),
-      home: Scaffold(
-        appBar: AppBar(title: Text('my souq')),
-        body: Center(child: const Text('demo home page')),
-      ),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: const AuthScreen(),
+
+      // Scaffold(
+      // appBar: AppBar(title: Text('my souq')),
+      // body: Column(
+      //   children: [
+      //     Center(child: const Text('demo home page')),
+      //     Builder(
+      //       builder: (context) {
+      //         return ElevatedButton(
+      //           onPressed: () {
+      //             Navigator.pushNamed(context, AuthScreen.routeName);
+      //           },
+      //           child: const Text('go to auth screen'),
+      //         );
+      //       },
+      //     ),
+      //   ],
+      // ),
     );
   }
 }

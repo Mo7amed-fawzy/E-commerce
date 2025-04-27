ecf6e488185371615ad169bdfcd8b22707565c17
Mohamed Fawzy <mohamed_2100699@science.sohag.edu.eg>
2025-04-28 00:08:47 +0300

    Add login functionality to the backend

--- back_end/routes/auth.js
+++ back_end/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('../models/user');
 const authRouter = express.Router();
 bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // authRouter.get('/user', (req, res) => {
 //     res.json({ msg: "M.Fawzy" });
@@ -13,13 +14,13 @@ authRouter.post('/api/signup', async (req, res) => {
     const { name, email, password } = req.body;
 
     if (!name || !email || !password) {
-        return res.status(400).json({ message: 'Please provide name, email, and password' });
+        return res.status(400).json({ msg1: 'Please provide name, email, and password' });
     }
 
     try {
         const existingUser = await User.findOne({ email });
         if (existingUser) {
-            return res.status(400).json({ message: 'User already exists' });
+            return res.status(400).json({ msg2: 'and Back-End Message is :User already exists' });
         }
 
         // Hash the password before saving it to the database (before making my modul)
@@ -28,12 +29,55 @@ authRouter.post('/api/signup', async (req, res) => {
 
         let user = new User({ name, email, password: hpassword });
         user = await user.save();
-        res.status(201).json({ message: 'User created successfully', user });
+        res.status(201).json({ msgSuc: 'User created successfully', user });
 
     } catch (error) {
         console.error('Error during signup:', error);
-        res.status(500).json({ message: 'Internal server error' });
+        // res.status(500).json({ error: 'Internal server error' });
+        res.status(500).json({ error: 'Email : your email is not valid' });
     }
+});
+
+
+authRouter.post('/api/signin', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ failLogMsg: '(Email) user is not exist ! ' });
+        }
+        const isMatch = await bcryptjs.compare(password, user.password, (err, result) => {
+            // if (err) {
+            //     return res.status(500).json({ error: 'Password is not correct' });
+            // }
+            // if (result) {
+            //     return res.status(200).json({ sucmsg: 'Login successful', user: User });
+            // } else {
+            //     return res.status(401).json({ failLogMsg: 'Invalid password' });
+            // }
+            if (isMatch) {
+                return res.status(200).json({ sucmsg: 'Login successful', user: user });
+            }
+
+            const token = jwt.sign({ id: user._id }, "myKey");
+            res.json({
+                token,
+                ...user._doc, // Spread the user document to include all fields in the response
+                //or
+                // user: {
+                //     id: User._id,
+                //     name: User.name,
+                //     email: User.email,
+                //     address: User.address,
+                //     phone: User.phone,
+                //     type: User.type
+                // }
+            });
+        });
+    } catch (error) {
+        console.error('Error during signin:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    };
 });
 
 
